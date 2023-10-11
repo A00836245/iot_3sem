a20f3db84087480b7c343e207539f5d0c8761cc9
Mariana Martínez <141279544+A00836245@users.noreply.github.com>
2023-10-11 12:13:40 -0600

    Update Codigo.sql

--- DATABASE/Codigo.sql
+++ DATABASE/Codigo.sql
@@ -1,3 +1,9 @@
 desc datos; // Conocer las filas;
 SELECT * FROM tablaproyectos; //Conocer todos los datos
 UPDATE microcontrolador SET ubicacion = 'SanNico' Where idMicro = 5; // Cambiar datos especificos
+
+//Contar sensor por microcontrolador
+SELECT MC, 
+COUNT(MC) AS sensores
+FROM sensores
+GROUP BY MC
